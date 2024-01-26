e2912550a53d30da0646f431f75e530e650737db
Polina <polina.nikitenko@kinoafisha.info>
2024-01-26 15:59:38 +0300

    add simple calculator task

---
+++ src/main/java/ru/ibs/sreda/SimpleCalculatorTask.java
@@ -0,0 +1,45 @@
+package ru.ibs.sreda;
+
+import java.util.Scanner;
+
+public class SimpleCalculatorTask {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ввод первого числа: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Ввод второго числа: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Ввод оператора +, -, * или / : ");
+        String operator = input.next();
+
+        int result = 0;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("На ноль не делим");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Это мы не проходили, это нам не задавали");
+                return;
+        }
+        System.out.println("Результат: " + result);
+    }
+}
